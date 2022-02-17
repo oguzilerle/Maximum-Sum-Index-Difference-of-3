44b7981e50d2d5c20a215584f9a0f073fcfc6671
oguzilerle <56355527+oguzilerle@users.noreply.github.com>
2022-02-17 11:11:03 +0000

    added cpp file

---
+++ Maximum Sum of close indexes.cpp
@@ -0,0 +1,30 @@
+#include "the4.h"
+
+
+
+int recursive_sln(int i, int*& arr, int &number_of_calls){ //direct recursive
+    number_of_calls+=1;
+
+    //your code here
+
+    return 0; // this is a dummy return value. YOU SHOULD CHANGE THIS!
+}
+
+
+
+int memoization_sln(int i, int*& arr, int*& mem){ //memoization
+
+    //your code here
+
+    return 0; // this is a dummy return value. YOU SHOULD CHANGE THIS!
+}
+
+
+
+int dp_sln(int size, int*& arr, int*& mem){ //dynamic programming
+
+    //your code here
+
+    return 0; // this is a dummy return value. YOU SHOULD CHANGE THIS!
+}
+
